94652aad15743890d798724d39ae923193bb6622
BweepBwop <a.kelsey.magat@gmail.com>
2024-07-24 21:46:32 +0800

    Create Day3:Arrays.js

---
+++ Day3:Arrays.js
@@ -0,0 +1,35 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+// Solution:
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getSecondLargest(nums) {
+  return [...new Set(nums)].sort((a, b) => a - b).slice(-2)[0];
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
